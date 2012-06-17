684d6cedc9abe0e393684cf97260c7ac837f35fa
Johnny Wall <jwall@hra.com>
2012-06-17 08:52:45 -0700

    Teach Learn Method to normalize linsense Data Better linear line learn, based on qualifying min on width expectations

--- src/dCamera_Lineal.c
+++ src/dCamera_Lineal.c
@@ -24,7 +24,7 @@ extern const struct CAR_CAL cal;
 /*************************************************************/
 
 
-
+#define noDerv 1
 
 		
 			
@@ -39,6 +39,7 @@ extern const struct CAR_CAL cal;
 
 void taskUpdateCamera(void)
 {
+
 	if(cameraTaskState==1)
 	{
 		
@@ -49,10 +50,14 @@ void taskUpdateCamera(void)
 			{			
 			};
 			
-        if(iCamera<64)
-			car.sensor.array[iCamera] = (U16)(ADC.CDR[0].B.CDATA );// + (64-i)/8;
-		else
-			car.sensor.array[iCamera] = (U16)(ADC.CDR[0].B.CDATA );// + (i-64)/8;
+		if(car.sensor.teachDone>0)
+		{
+			car.sensor.array[iCamera] = (U16)(ADC.CDR[0].B.CDATA ) * 256 / car.sensor.arrayTeach[iCamera];
+		}
+		else		
+		{		
+			car.sensor.array[iCamera] = (U16)(ADC.CDR[0].B.CDATA );// + (U16)(iCamera-64)/2;
+		}
 
 		if((iCamera>2) & iCamera < 126)  //if we are in edgeless zone, lets find the min and max,
 			{	
@@ -78,6 +83,7 @@ void taskUpdateCamera(void)
 	cameraTaskState=1;
 	}
 	
+
 }
 
 void taskUpdateCameraStart(void)
@@ -107,49 +113,72 @@ void taskUpdateCameraStart(void)
 void taskUpdateCameraEnd(void)
 {
 	U8 i;
+	U8 modeFind=0;
 	U32 avgSum = 0;
-	S16 derive = 0;
+	S8 derive = 0;
 	
 	PIT.CH[2].TCTRL.B.TEN = 0;    /* MPC56xxB/P/S: Clear PIT 1 flag by writing 1 */
 	
+	disableIrq();		   	/* Ensure INTC current prority=0 & enable IRQ */
 	car.sensor.threshold = (car.sensor.valMax - car.sensor.valMin)/2;	
 	car.sensor.cornLeft = minPosCamera;
 	car.sensor.cornRight = minPosCamera;
 	
-  	while(((car.sensor.array[car.sensor.cornLeft])<(car.sensor.valMin+car.sensor.threshold)) && (car.sensor.cornLeft>2))
+	i=6;
+	while(i<122)
+	{
+		if(modeFind==0)
+		{
+			if(car.sensor.array[i] < car.sensor.threshold)
+			{
+			car.sensor.cornLeft = i;
+			modeFind =1;	
+			}
+		}
+		else if(modeFind==1)
+		{
+			if(car.sensor.array[i] > car.sensor.threshold)
+			{
+			if(( (i - car.sensor.cornLeft) > car.sensor.TeachSenseWidthMin) && ((i - car.sensor.cornLeft) < car.sensor.TeachSenseWidthMax))	
+				car.sensor.cornRight = i;
+			modeFind = 2;
+			}
+			
+		}
+		
+		i++;
+		
+	}
+	/*
+  	while(((car.sensor.array[car.sensor.cornLeft])<(car.sensor.valMin+car.sensor.threshold)) && (car.sensor.cornLeft>10))
 		{
 		car.sensor.cornLeft--;	
 		}
 		
 		
-	while(((car.sensor.array[car.sensor.cornRight])<(car.sensor.valMin+car.sensor.threshold)) && (car.sensor.cornRight<126))
+	while(((car.sensor.array[car.sensor.cornRight])<(car.sensor.valMin+car.sensor.threshold)) && (car.sensor.cornRight<118))
 		{
 		car.sensor.cornRight++;	
 		}
+*/
 
-	car.sensor.width = car.sensor.cornRight - car.sensor.cornLeft;
+	#ifndef noDerv
 	
-	car.sensor.c4 = car.sensor.c3;
-	car.sensor.c3 = car.sensor.c2;
-	car.sensor.c2 = car.sensor.c1;
-	car.sensor.c1 = car.sensor.center;
+	car.sensor.deriveValMax = -100;
+	car.sensor.deriveValMin = 100;
+	car.sensor.deriveCornLeft = 10;
+	car.sensor.deriveCornRight = 118;
 	
-	car.sensor.center = (car.sensor.cornRight + car.sensor.cornLeft)/2;
-
-	
-	car.sensor.deriveValMax = -30000;
-	car.sensor.deriveValMin = 30000;
-	car.sensor.deriveCornLeft = 0;
-	car.sensor.deriveCornRight = 127;
 	//averaging Method
-	for(i=1;i<126;i++)
+	for(i=10;i<118;i++)
 	{		
-		derive = (derive + (S16)car.sensor.array[i] - (S16)car.sensor.array[i-1])/2; //averaged derivative
+		derive = (S8)(((S16)derive*5 +(S16)car.sensor.array[i] - (S16)car.sensor.array[i-1])/6); //averaged derivative
 		car.sensor.arrayDerive[i] = derive;
 			
 		if(car.sensor.deriveValMin > car.sensor.arrayDerive[i]) //if a new min is found, latch it
 			{
 			car.sensor.deriveValMin = car.sensor.arrayDerive[i];
+			car.sensor.deriveValMax = -100; //reset max find for RHS
 			car.sensor.deriveCornLeft = i;								// set the mim pos
 			}
 		if(car.sensor.deriveValMax < car.sensor.arrayDerive[i])
@@ -159,12 +188,57 @@ void taskUpdateCameraEnd(void)
 			} 		
 	}
 	
-	//A tap delay to help average the center measured position
-	//car.sensor.center = (car.sensor.center * 4 + car.sensor.c1 * 3 + car.sensor.c2 * 2 + car.sensor.c3)/10;
+	car.sensor.deriveWidth = car.sensor.deriveCornRight - car.sensor.deriveCornLeft;
+	#endif
+	car.sensor.width = car.sensor.cornRight - car.sensor.cornLeft;
+	
+	car.sensor.c4 = car.sensor.c3;
+	car.sensor.c3 = car.sensor.c2;
+	car.sensor.c2 = car.sensor.c1;
+	car.sensor.c1 = car.sensor.center;
 	
 
-	if((car.sensor.width >= cal.senseWidthMin) && (car.sensor.width <= cal.senseWidthMax) && (car.sensor.threshold >= cal.sensorMinDynRange))
+		#ifndef noDerv
+	//A tap delay to help average the center measured position
+	if((car.sensor.deriveWidth >= cal.senseWidthMinDer) && (car.sensor.deriveWidth <= cal.senseWidthMaxDer)) //derivOKay
 	{
+		if(modeFind==2) //stokay found both
+		{
+			
+		
+		car.sensor.center = (car.sensor.cornRight + car.sensor.cornLeft + car.sensor.deriveCornRight + car.sensor.deriveCornLeft)/4;
+
+		car.ctrl.error =  (S16)((car.ctrl.error + ((S16)car.sensor.center-car.ctrl.controlCenter)))/2;
+		
+		if(car.ctrl.error<-cal.sensorMaxError)
+			car.ctrl.error = (S16)-cal.sensorMaxError;
+		else if(car.ctrl.error>cal.sensorMaxError)
+			car.ctrl.error = (S16)cal.sensorMaxError;
+		
+		
+		car.sensor.valid = 3;
+		}
+		else
+		{
+		car.sensor.center = (car.sensor.deriveCornRight + car.sensor.deriveCornLeft)/2;
+
+		car.ctrl.error =  (S16)((car.ctrl.error + ((S16)car.sensor.center-car.ctrl.controlCenter)))/2;
+		
+		if(car.ctrl.error<-cal.sensorMaxError)
+			car.ctrl.error = (S16)-cal.sensorMaxError;
+		else if(car.ctrl.error>cal.sensorMaxError)
+			car.ctrl.error = (S16)cal.sensorMaxError;
+		
+		
+		car.sensor.valid = 2;	
+			
+		}
+	
+	}
+	else if((modeFind==2) && (car.sensor.threshold >= car.sensor.TeachSensorMinDynRange))
+	{
+		car.sensor.center = (car.sensor.cornRight + car.sensor.cornLeft)/2;
+
 		car.ctrl.error =  (S16)((car.ctrl.error + ((S16)car.sensor.center-car.ctrl.controlCenter)))/2;
 		
 		if(car.ctrl.error<-cal.sensorMaxError)
@@ -177,11 +251,55 @@ void taskUpdateCameraEnd(void)
 	}
 	else
 		car.sensor.valid = 0;
-
 	
+	#else
+	
+	if((car.sensor.threshold >= car.sensor.TeachSensorMinDynRange) && (modeFind==2))
+		{
+		car.sensor.center = (car.sensor.cornRight + car.sensor.cornLeft)/2;
 
+		car.ctrl.error =  (S16)((car.ctrl.error + ((S16)car.sensor.center-car.ctrl.controlCenter)))/2;
+		
+		if(car.ctrl.error<-cal.sensorMaxError)
+			car.ctrl.error = (S16)-cal.sensorMaxError;
+		else if(car.ctrl.error>cal.sensorMaxError)
+			car.ctrl.error = (S16)cal.sensorMaxError;
+		
+		
+		car.sensor.valid = 1;
+	}
+	else
+		car.sensor.valid = 0;
+	#endif
 
-
+ 
+	if(((SIU.PGPDI[2].R & 0x20000000) == 0x0000000) && car.sensor.teachDone==0) // button 3 pressed, teach
+	{
+	i=0;
+		while(i<128)
+		{
+			car.sensor.arrayTeach[i] = 	car.sensor.array[i] ;
+			i++;
+		}
+		car.sensor.teachDone = 1;
+		
+	}
+	
+	
+	if(((SIU.PGPDI[2].R & 0x40000000) == 0x0000000) & car.sensor.teachDone==1) // button 2 pressed, teach line chars
+	{
+	if(car.sensor.width < 8)
+		car.sensor.TeachSenseWidthMin = car.sensor.width/2;
+	else
+		car.sensor.TeachSenseWidthMin = car.sensor.width-7;
+	car.sensor.TeachSenseWidthMax = car.sensor.width+7;
+	car.sensor.TeachSensorMinDynRange = car.sensor.threshold * 9 / 16;
+		car.sensor.teachDone = 2;
+		
+	}
+	
+	taskPIDupdate();
+	enableIrq();		   	/* Ensure INTC current prority=0 & enable IRQ */
 	
 }
 
@@ -265,7 +383,7 @@ void u8Capture_Pixel_Values(void)
 	//car.sensor.center = (car.sensor.center * 4 + car.sensor.c1 * 3 + car.sensor.c2 * 2 + car.sensor.c3)/10;
 	
 
-	if((car.sensor.width >= cal.senseWidthMin) && (car.sensor.width <= cal.senseWidthMax) && (car.sensor.threshold >= cal.sensorMinDynRange))
+	if((car.sensor.width >= car.sensor.TeachSenseWidthMin) && (car.sensor.width <= car.sensor.TeachSenseWidthMax) && (car.sensor.threshold >= car.sensor.TeachSensorMinDynRange))
 	{
 		car.ctrl.error =  (S16)((car.ctrl.error + ((S16)car.sensor.center-car.ctrl.controlCenter)))/2;
 		
